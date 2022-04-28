f13b4c243d5ef845332160ae700cab2c2403fb65
Allan <allangeek@gmail.com>
2022-04-28 18:00:07 -0300

    Finaliza projeto

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -11,50 +11,109 @@ interface FormAddImageProps {
   closeModal: () => void;
 }
 
+interface ImageData {
+  url: string;
+  title: string;
+  description: string;
+}
+
 export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   const [imageUrl, setImageUrl] = useState('');
   const [localImageUrl, setLocalImageUrl] = useState('');
   const toast = useToast();
 
+  const acceptedFormatsRegex =
+    /(?:([^:/?#]+):)?(?:([^/?#]*))?([^?#](?:jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?/g;
+
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      // REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: 'Arquivo é obrigatório',
+      validate: {
+        lassThan10MB: file =>
+          file[0].size < 10000000 || 'O arquivo precisa ser menor que 10MB',
+
+        acceptedFormats: file =>
+          acceptedFormatsRegex.test(file[0].type) ||
+          'Os arquivos precisam ser do tipo JPEG, PNG ou GIF',
+      },
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      // REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: 'O título é obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres',
+      },
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      // REQUIRED, MAX LENGTH VALIDATIONS
+      required: 'A descrição é obrigatória',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres',
+      },
     },
   };
 
   const queryClient = useQueryClient();
   const mutation = useMutation(
-    // TODO MUTATION API POST REQUEST,
+    // MUTATION API POST REQUEST,
+    async (image: ImageData) => {
+      await api.post('/api/images', {
+        ...image,
+        url: imageUrl,
+      });
+    },
     {
-      // TODO ONSUCCESS MUTATION
+      // ONSUCCESS MUTATION
+      onSuccess: () => {
+        queryClient.invalidateQueries('images');
+      },
     }
   );
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState,
-    setError,
-    trigger,
-  } = useForm();
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
   const { errors } = formState;
 
-  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+  const onSubmit = async (data: ImageData): Promise<void> => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
-      // TODO EXECUTE ASYNC MUTATION
-      // TODO SHOW SUCCESS TOAST
+      // SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+      if (!imageUrl) {
+        toast({
+          status: 'error',
+          title: 'Imagem não adicionada',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.',
+        });
+      }
+      // EXECUTE ASYNC MUTATION
+      await mutation.mutateAsync(data);
+
+      // SHOW SUCCESS TOAST
+      toast({
+        status: 'success',
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+      });
     } catch {
-      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      // SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        status: 'error',
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.',
+      });
     } finally {
-      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      // CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      setImageUrl('');
+      setLocalImageUrl('');
+      closeModal();
     }
   };
 
@@ -67,20 +126,26 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          // SEND IMAGE ERRORS
+          error={errors.image}
+          // REGISTER IMAGE INPUT WITH VALIDATIONS
+          {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          // SEND TITLE ERRORS
+          error={errors.title}
+          // REGISTER TITLE INPUT WITH VALIDATIONS
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          // SEND DESCRIPTION ERRORS
+          error={errors.description}
+          // REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,32 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+// import { string } from 'yup';
+
+interface Image {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
 
 export default function Home(): JSX.Element {
+  async function getImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -18,18 +42,29 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    // AXIOS REQUEST WITH PARAM
+    getImages,
+    // GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    // FORMAT AND FLAT DATA ARRAY
+    const finalData = data?.pages.flatMap(image => image.data.flat());
+    return finalData;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  // RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  // RENDER ERROR SCREEN
+  if (isError && !isLoading) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +72,18 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {
+          /* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */
+          hasNextPage && (
+            <Button
+              onClick={() => fetchNextPage()}
+              disabled={isFetchingNextPage}
+              mt="6"
+            >
+              {!isFetchingNextPage ? 'Carregar mais' : 'Carregando...'}
+            </Button>
+          )
+        }
       </Box>
     </>
   );
